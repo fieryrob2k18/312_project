99da7068715595c2c30cfd5c141558e4301f6637
Quinn Tarbell <Qmtfive@users.noreply.github.com>
2022-03-16 11:47:42 -0400

    Added newlines bc rin is ocd

--- mongo.py
+++ mongo.py
@@ -6,18 +6,23 @@ class MongoDB:
         client = MongoClient(url)
         database = client[dbName]
         self.collection = database[collName]
+
     def addOne(self, id, body):
         r = self.collection.insert_one(json.loads(body))
         return json.dumps(r)
+
     def addMany(self, id, body):
         r = self.collection.insert_many(json.loads(body))
         return json.dumps(r)
+
     def getOne(self, id, body):
         r = self.collection.find_one({"_id": id})
         return json.dumps(r)
+
     def updateOne(self, id, body):
         r = self.collection.update_one({"_id": id}, json.loads(body))
         return json.dumps(r)
+
     def removeOne(self, id, body):
         r = self.collection.delete_one({"_id": id})
         return json.dumps(r)
