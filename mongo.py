061054695a8b568138075ac4341fd82bcec949de
Quinn Tarbell <Qmtfive@users.noreply.github.com>
2022-03-16 11:26:06 -0400

    Create mongo.py

---
+++ mongo.py
@@ -0,0 +1,23 @@
+from pymongo import MongoClient
+import json
+
+class MongoDB:
+    def __init__(self, url, dbName, collName):
+        client = MongoClient(url)
+        database = client[dbName]
+        self.collection = database[collName]
+    def addOne(self, id, body):
+        r = self.collection.insert_one(json.loads(body))
+        return json.dumps(r)
+    def addMany(self, id, body):
+        r = self.collection.insert_many(json.loads(body))
+        return json.dumps(r)
+    def getOne(self, id, body):
+        r = self.collection.find_one({"_id": id})
+        return json.dumps(r)
+    def updateOne(self, id, body):
+        r = self.collection.update_one({"_id": id}, json.loads(body))
+        return json.dumps(r)
+    def removeOne(self, id, body):
+        r = self.collection.delete_one({"_id": id})
+        return json.dumps(r)
