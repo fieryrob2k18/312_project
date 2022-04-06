a972a15b186e5ec6d39cb3092e21571da7dffdee
Robert <rbamm@buffalo.edu>
2022-04-06 11:14:06 -0400

    basic template code working #10

--- template.py
+++ template.py
@@ -18,5 +18,49 @@
 
 #Takes html data as input, replace all with html strings
 #returns modified html string
+
+#sample functions for testing html template functionality
+def test1_func():
+    return "<h1>This text made by html template{{}}</h1>"
+
+
+#dictionary for storing html template functions
+tmpl_dict = {"TEST1": test1_func}
+
+
+#Replace template vars in html with output
+#Note that while object is a byte list,
+#It does get briefly converted to a string in this function
 def renderHtmlTemplate(html):
-    return html
+    #convert to string for string parsing funcs
+    #splitting like this avoids replacing user input with {{}}
+    #Since each part is looked at only once
+    #html is assumed to always be a bytes object
+    html_str = html.decode("utf-8")
+    parsed_html = html_str.split("{{")
+    #we'll reuse html_str for the output
+    html_str = ""
+
+    for section in parsed_html:
+        #check if the key was found
+        key_replaced = False
+        for key in tmpl_dict:
+            replace_val = key + "}}"
+            if section.startswith(replace_val):
+                key_replaced = True
+                output = tmpl_dict[key]()
+                section = section.replace(replace_val, output)
+                #under this format there is only one key per section
+                break
+        #if the value wasn't a proper one, replace with empty str
+        #(or there is no key, like at the beginning of a file)
+        if not key_replaced:
+            idx = section.find("}}")
+            if idx > -1:
+                #add the 2 to remove brackets as well
+                html_str += section[idx+2:]
+            else:
+                html_str += section
+        else:
+            html_str += section
+    return bytes(html_str, "utf-8")
