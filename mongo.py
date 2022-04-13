f4d391cd3660c8d0e800fe795f20ad2e7c64d5b1
Quinn Tarbell <Qmtfive@users.noreply.github.com>
2022-04-12 22:35:46 -0400

    getMany and getAll methods

--- mongo.py
+++ mongo.py
@@ -7,22 +7,30 @@ class MongoDB:
         database = client[dbName]
         self.collection = database[collName]
 
-    def addOne(self, id, body):
+    def addOne(self, body):
         r = self.collection.insert_one(json.loads(body))
         return json.dumps(r)
 
-    def addMany(self, id, body):
+    def addMany(self, body):
         r = self.collection.insert_many(json.loads(body))
         return json.dumps(r)
 
-    def getOne(self, id, body):
-        r = self.collection.find_one({"_id": id})
+    def getOne(self, id):
+        r = list(self.collection.find_one({"_id": id}))
+        return json.dumps(r)
+    
+    def getMany(self, key, value):
+        r = list(self.collection.find({key: value}))
+        return json.dumps(r)
+
+    def getAll(self):
+        r = self.collection.find({})
         return json.dumps(r)
 
     def updateOne(self, id, body):
         r = self.collection.update_one({"_id": id}, json.loads(body))
         return json.dumps(r)
 
-    def removeOne(self, id, body):
+    def removeOne(self, id):
         r = self.collection.delete_one({"_id": id})
         return json.dumps(r)
