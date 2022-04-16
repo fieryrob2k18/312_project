bb6d77077dce2e4c969281d4a5fd9dde83b70802
Quinn Tarbell <Qmtfive@users.noreply.github.com>
2022-04-15 20:20:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import socket
+import socketserver
 from black import main
 import webserver
 
@@ -6,21 +6,25 @@ import webserver
 #I can move it if need be - Rob
 DEBUG = True
 
-def main():
-    #TODO: Should I move all this to the webserver file
-    #and just make one function call? - Rob
-    soc = socket.socket()
-    soc.bind(('0.0.0.0', 8000))
 
-    #5 is "standard" connection # according to web tutorials
-    soc.listen(5)
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    """
+    The request handler class for our server.
 
-    while True:
-        conn, addr = soc.accept()
-        if DEBUG:
-            print("New Connection from: ", addr)
-        webserver.handle_conn(conn)
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
 
+    def handle(self):
+        webserver.handle_conn(self.request)
 
 if __name__ == "__main__":
-    main()
+    HOST, PORT = "0.0.0.0", 8000
+
+    # Create the server, binding to localhost on port 8000
+    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
+        print("Server started!", flush=True)
+        # Activate the server; this will keep running until you
+        # interrupt the program with Ctrl-C
+        server.serve_forever()
